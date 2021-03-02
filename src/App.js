2a93845f078b71375defb4b6cdb1efac97e1ba00
Duriumm <lasse_katten@hotmail.com>
2021-03-02 15:15:50 +0100

    Test with deploying webpage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div
       className="App"
-      style={{ backgroundColor: "#FFEAD0" }}
+      // style={{ backgroundColor: "#FFEAD0" }}
     >
 
       <ButtonBar />
